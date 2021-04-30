9f00afb74fc36b47df065aaf601a47ac498acfb0
Kevin O'Neil <kevinmoneil@live.com>
2021-04-30 16:42:08 -0700

    Changes return statement

--- src/Input.tsx
+++ src/Input.tsx
@@ -3,7 +3,5 @@ import React, { forwardRef } from 'react'
 interface Props extends React.HTMLProps<HTMLInputElement> {}
 
 export const Input = forwardRef<HTMLInputElement, Props>((props, ref) => {
-  return (
-    <input ref={ref} {...props} />
-  )
+  return <input ref={ref} {...props} />
 })
