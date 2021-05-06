54cb24a43cdc9cb04208d062c012f41c63fb6d97
Kevin O'Neil <kevinmoneil@live.com>
2021-05-06 00:03:45 -0700

    Takes advantage of composition

--- src/Autocomplete.tsx
+++ src/Autocomplete.tsx
@@ -1,20 +1,36 @@
-import React, { useRef } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import { AutocompleteInput } from './AutocompleteInput'
 import { AutocompleteList } from './AutocompleteList'
 import { AutocompleteEntry } from './AutocompleteEntry'
+import { Dropdown } from './Dropdown'
 
 export const Autocomplete = () => {
-  const ref = useRef<HTMLInputElement>(null)
+  const inputRef = useRef<HTMLInputElement>(null)
+  const [inputValue, setInputValue] = useState('');
+  const [showDropdown, setShowDropdown] = useState(false)
   const results = ['beer', 'hummus', 'candy']
   const ariaGroup = 'group-0'
 
+  useEffect(() => {
+    if (inputRef?.current.value.length > 0) {
+      setShowDropdown(true);
+    } else {
+      setShowDropdown(false)
+    }
+  }, [inputRef.current?.value, setShowDropdown]);
+
   return (
-    <AutocompleteInput ref={ref} results={results}>
-      <AutocompleteList ariaResultsText={`${results.length} results found`} headerText="Recent Entries" ariaGroupHeader={`${ariaGroup}-header`}>
-        {results.map((result, index) => (
-          <AutocompleteEntry  key={result} id={`${ariaGroup}-option-${index}`}>{result}</AutocompleteEntry>
-        ))}
-      </AutocompleteList> 
-    </AutocompleteInput>
+    <>
+      <AutocompleteInput value={inputValue} setInputValue={setInputValue} ref={inputRef} results={results} />
+      {showDropdown && (
+        <Dropdown>
+          <AutocompleteList ariaResultsText={`${results.length} results found`} headerText="Recent Entries" ariaGroupHeader={`${ariaGroup}-header`}>
+            {results.map((result, index) => (
+              <AutocompleteEntry  key={result} id={`${ariaGroup}-option-${index}`}>{result}</AutocompleteEntry>
+            ))}
+          </AutocompleteList> 
+        </Dropdown>
+      )}
+    </>
   )
 }
--- src/AutocompleteInput.tsx
+++ src/AutocompleteInput.tsx
@@ -1,55 +1,33 @@
-import React, { useState, useEffect, forwardRef } from 'react';
-import { Input } from './Input';
-import { Dropdown } from './Dropdown';
+import React, { forwardRef, HTMLProps } from 'react';
 
 import { useHighlightedEntry } from './hooks/useHighlighedEntry'
 
 interface Props {
   results: string[]
-  dropdownClassName?: string
-  children: JSX.Element | JSX.Element[];
+  setInputValue: (entry: string) => void
 }
 
 export const AutocompleteInput = forwardRef<HTMLInputElement, Props>((props, ref) => {
-  const { results, children, ...options } = props;
+  const { results, setInputValue, ...options } = props;
   const { getPreviousEntry, getNextEntry } = useHighlightedEntry(results)
 
-  const [inputValue, setInputValue] = useState('');
-  const [showDropdown, setShowDropdown] = useState(false);
-
-  useEffect(() => {
-    if (inputValue?.length > 0) {
-      setShowDropdown(true);
-    } else {
-      setShowDropdown(false)
-    }
-  }, [inputValue, setShowDropdown]);
-
   return (
-    <>
-      <Input 
-        {...options}
-        ref={ref}
-        value={inputValue} 
-        onKeyDown={(event) => {
-          if (event.key === 'ArrowUp') {
-            const entry = getPreviousEntry()
-            setInputValue(entry)
-          }
-          if (event.key === 'ArrowDown') {
-            const entry = getNextEntry()
-            setInputValue(entry)
-          }
-        }}
-        onChange={(event) => { 
-          setInputValue(event.currentTarget.value);
-        }}
-      />
-      {showDropdown && (
-        <Dropdown className={options?.dropdownClassName}>
-          {children}
-        </Dropdown>
-      )}
-    </>
+    <input 
+      ref={ref}
+      onKeyDown={(event) => {
+        if (event.key === 'ArrowUp') {
+          const entry = getPreviousEntry()
+          setInputValue(entry)
+        }
+        if (event.key === 'ArrowDown') {
+          const entry = getNextEntry()
+          setInputValue(entry)
+        }
+      }}
+      onChange={(event) => { 
+        setInputValue(event.currentTarget.value);
+      }}
+      {...options}
+    />
   )
 })
