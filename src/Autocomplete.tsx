aa2e3ec7ad4c78933abfa2aa2a9de19e74ae59a9
Kevin O'Neil <kevinmoneil@live.com>
2021-05-06 00:13:11 -0700

    Adds new hook

--- src/Autocomplete.tsx
+++ src/Autocomplete.tsx
@@ -3,21 +3,14 @@ import { AutocompleteInput } from './AutocompleteInput'
 import { AutocompleteList } from './AutocompleteList'
 import { AutocompleteEntry } from './AutocompleteEntry'
 import { Dropdown } from './Dropdown'
+import { useShowDropdown } from './hooks/useShowDropdown'
 
 export const Autocomplete = () => {
   const inputRef = useRef<HTMLInputElement>(null)
   const [inputValue, setInputValue] = useState('');
-  const [showDropdown, setShowDropdown] = useState(false)
+  const showDropdown = useShowDropdown(inputRef)
   const results = ['beer', 'hummus', 'candy']
   const ariaGroup = 'group-0'
-
-  useEffect(() => {
-    if (inputRef?.current.value.length > 0) {
-      setShowDropdown(true);
-    } else {
-      setShowDropdown(false)
-    }
-  }, [inputRef.current?.value, setShowDropdown]);
 
   return (
     <>
---
+++ src/hooks/useShowDropdown.ts
@@ -0,0 +1,15 @@
+import { useState, useEffect } from 'react'
+
+export const useShowDropdown = (inputRef) => {
+  const [showDropdown, setShowDropdown] = useState(false);
+
+  useEffect(() => {
+    if (inputRef?.current.value.length > 0) {
+      setShowDropdown(true);
+    } else {
+      setShowDropdown(false);
+    }
+  }, [inputRef.current?.value, setShowDropdown]);
+
+  return showDropdown;
+}
