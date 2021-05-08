5ee04cfce3f46491c6a6090a523228dfc08a5fff
Kevin O'Neil <kevinmoneil@live.com>
2021-05-08 01:27:52 -0700

    Updates test description

--- src/__tests__/Autocomplete.test.tsx
+++ src/__tests__/Autocomplete.test.tsx
@@ -1,21 +1,21 @@
-import React, { useRef } from "react";
+import React from "react";
 import { render } from "@testing-library/react";
 
 import { AutocompleteInput } from "../AutocompleteInput";
 import { AutocompleteList } from "../AutocompleteList";
 import { AutocompleteEntry } from "../AutocompleteEntry";
 
-test("Autocomplete does not break", () => {
-  const results = ["test", "test2"];
-
-  expect(() =>
-    render(
-      <>
-        <AutocompleteInput ref={() => {}} value="test" onChange={() => {}} />
-        <AutocompleteList>
-          <AutocompleteEntry>test</AutocompleteEntry>
-        </AutocompleteList>
-      </>
-    )
-  ).not.toThrow();
+describe("happy path", () => {
+  test("Autocomplete does not break when composing", () => {
+    expect(() =>
+      render(
+        <>
+          <AutocompleteInput ref={() => {}} value="test" onChange={() => {}} />
+          <AutocompleteList>
+            <AutocompleteEntry>test</AutocompleteEntry>
+          </AutocompleteList>
+        </>
+      )
+    ).not.toThrow();
+  });
 });
