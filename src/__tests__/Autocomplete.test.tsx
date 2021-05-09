d15414819a10bb5cd37794ece06a13ff4ad9ccad
Kevin O'Neil <kevinmoneil@live.com>
2021-05-09 13:21:04 -0700

    Removes unused import

--- src/__tests__/Autocomplete.test.tsx
+++ src/__tests__/Autocomplete.test.tsx
@@ -1,10 +1,5 @@
 import React from "react";
-import {
-  fireEvent,
-  getByPlaceholderText,
-  render,
-  screen,
-} from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 
 import {
   AutocompleteInput,
