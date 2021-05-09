dcd2472cc0e8a06e914562c6c3949cd32f936e5b
Kevin O'Neil <kevinmoneil@live.com>
2021-05-08 22:27:32 -0700

    Updates styles

--- docs/index.tsx
+++ docs/index.tsx
@@ -4,6 +4,12 @@ import { Autocomplete } from "./Autocomplete";
 
 const domContainer = document.querySelector("#app");
 
-const app = () => <Autocomplete />;
+const app = () => (
+  <div className="app-container">
+    <h1>Droopy Dropdown</h1>
+    <p>An toolkit for building a11y friendly dropdown components</p>
+    <Autocomplete />
+  </div>
+);
 
 ReactDOM.render(app(), domContainer);
