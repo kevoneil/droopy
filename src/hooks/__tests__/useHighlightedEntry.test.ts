bc52c8ff87587d51b0d614329dd32e25b45ed50c
Kevin O'Neil <kevinmoneil@live.com>
2021-05-14 21:06:32 -0700

    Adds another test for hook

--- src/hooks/__tests__/useHighlightedEntry.test.ts
+++ src/hooks/__tests__/useHighlightedEntry.test.ts
@@ -1,18 +1,38 @@
-import { renderHook, act } from '@testing-library/react-hooks';
-import useHighlightedEntry from '../useHighlightedEntry';
+import { renderHook, act } from "@testing-library/react-hooks";
+import useHighlightedEntry from "../useHighlightedEntry";
+
+enum ResultKey {
+  Recent = "recentSearches",
+  Popular = "popularSearches",
+}
 
 const results = {
-  recentSearches: ['test'],
+  [ResultKey.Recent]: ["candy", "beer", "hummus"],
+  [ResultKey.Popular]: ["wine", "cheese", "sushi", "curry"],
 };
 
-test('should increment highlighted entry index', () => {
-  const { result } = renderHook(() => useHighlightedEntry(results));
+describe("useHighlightedEntry hook", () => {
+  it("should increment highlighted entry index", () => {
+    const { result } = renderHook(() => useHighlightedEntry(results));
 
-  act(() => {
-    result.current.getNextEntry();
+    act(() => {
+      result.current.getNextEntry();
+    });
+
+    expect(result.current.highlightedIndex).toEqual(0);
+    expect(result.current.activeGroup).toEqual(ResultKey.Recent);
+    expect(result.current.activeGroupIndex).toEqual(0);
   });
 
-  expect(result.current.highlightedIndex).toEqual(0);
-  expect(result.current.activeGroup).toEqual('recentSearches');
-  expect(result.current.activeGroupIndex).toEqual(0);
+  it("should return last entry if user presses the up arrow on first entry", () => {
+    const { result } = renderHook(() => useHighlightedEntry(results));
+
+    act(() => {
+      result.current.getPreviousEntry();
+    });
+
+    expect(result.current.highlightedIndex).toEqual(6);
+    expect(result.current.activeGroup).toEqual(ResultKey.Popular);
+    expect(result.current.activeGroupIndex).toEqual(3);
+  });
 });
