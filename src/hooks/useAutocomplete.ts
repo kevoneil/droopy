d97008abda76a898ab6f430580169cf351b9e6b4
Kevin O'Neil <kevinmoneil@live.com>
2021-05-30 00:17:43 -0700

    Adds default to context provider

--- src/hooks/useAutocomplete.ts
+++ src/hooks/useAutocomplete.ts
@@ -6,6 +6,7 @@ type ContextReturnType = ReturnType<typeof useHighlightedEntry>;
 export const AutocompleteContext = createContext<ContextReturnType>({
   activeGroup: "",
   activeGroupIndex: -1,
+  activeGroupEntry: "",
   highlightedIndex: -1,
   getPreviousEntry: () => "",
   setHighlightedEntry: () => "",
